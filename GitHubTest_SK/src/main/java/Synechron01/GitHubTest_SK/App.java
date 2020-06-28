a825e3b39201927d19269f862d07e8072f3976bd
sateesh025 <kunapareddysateesh@gmail.com>
2020-06-28 08:52:07 +0530

    "branch commit"

---
+++ GitHubTest_SK/src/main/java/Synechron01/GitHubTest_SK/App.java
@@ -0,0 +1,13 @@
+package Synechron01.GitHubTest_SK;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
